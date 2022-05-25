e595a105aa00dd26a46cd35903b04081e45c23bb
Alex Flower <aazvetkov@gmail.com>
2022-05-25 12:00:34 +0300

    Initialization

---
+++ src/components/Timer.js
@@ -0,0 +1,16 @@
+import { setState } from 'react'
+
+
+export default function Timer() {
+  console.log('Render in Timer');
+  // const [time, setTime] = setState('')
+
+  // setTime('hello')
+
+  return (
+    <>
+      <div>Minutes: {true}</div>
+      <div>Seconds: {true}</div>
+    </>
+  )
+}
